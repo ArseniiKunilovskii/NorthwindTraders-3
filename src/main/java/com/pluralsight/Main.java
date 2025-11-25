c50d87fff5d57cfa9db2540d44dcae706ee86085
arsenii <arseniy.kunilovskiy@gmail.com>
2025-11-25 15:25:31 -0500

    minor upgrade

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -5,6 +5,11 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
+        if(args.length!=2){
+            System.out.println("Please provide username and password");
+            System.exit(1);
+        }
+
         String username = args[0];
         String password = args[1];
 
